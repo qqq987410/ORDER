3b3a4133b42ae2a22c7fb7e939e726838b32da40
hsuweiwei <qqq987410@gmail.com>
2020-11-25 14:21:05 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import styles from "./App.module.css";
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1 className={styles.h1}>Hello~</h1>
+    </div>
+  );
+}
+
+export default App;
